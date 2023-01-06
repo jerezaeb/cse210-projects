2bb285fe1df8147368e94b2f8982baf14da85739
jerezaeb <jerezaespi@gmail.com>
2023-01-06 20:02:01 +0800

    update prep1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade in percentage? ");
+        string grade = Console.ReadLine ();
+        int Grade =  int.Parse(grade);
+        
+        string letter = "";
+
+
+        if (Grade >= 90)
+        {
+            letter = "A";
+        }
+
+        if (Grade >= 80 && Grade <90)
+        {
+            letter = "B";
+        }
+
+        if (Grade >= 70 && Grade <80) 
+        {
+            letter = "C";
+        }
+
+        if (Grade >= 60 && Grade <70) 
+        {
+            letter = "D";
+        }
+
+        else 
+        {
+            letter = "F";
+        }
+
+    
+        Console.WriteLine ($"Your grade is {letter}");
+        
+
     }
 }
