b132c01e1e97f9d5c79d329c7be2474f956e530a
jerezaeb <jerezaespi@gmail.com>
2023-01-11 23:38:31 +0800

    save prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,9 +6,9 @@ class Program
     {   string game= "";
         while (game == "yes")
         {  
-        Console.Write("Do you want to play? ");
+        Console.WriteLine("Do you want to play? ");
         game = Console.ReadLine();
-
+        
             Random rnd = new Random();
             int magicNumber =  rnd.Next(100);
             int guess = 0;
